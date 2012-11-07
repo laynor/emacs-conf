d6a4b1061be3240a0b36c1ea5534d3098abda444
Alessandro Piras <laynor@gmail.com>
2012-11-07 11:58:01 +0100

    Added auto-complete.

---
+++ packages/sm-package-popup.el
@@ -0,0 +1,8 @@
+;;;; Package popup
+(sm-package popup
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package popup)
