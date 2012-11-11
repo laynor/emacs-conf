5908c37273a3f180aec4032ecda616471e47c877
Alessandro Piras <laynor@gmail.com>
2012-11-11 21:09:56 +0100

    Updated windows profile

--- profiles/home-windows.el
+++ profiles/home-windows.el
@@ -6,12 +6,13 @@
   )
 
 ;;; Modules to activate
-(sm-require-modules "base")
+(sm-require-modules "base" "elisp")
 
 ;;; After loading modules
 (sm-profile-post (home-windows)
   (setq magit-git-executable "c:/cygwin/bin/git.exe")
   (load "custom.el")
+  (add-hook 'kill-emacs-hook 'sm-recompile-all)
   (custom-set-faces
    '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "outline" :family "Consolas"))))))
 
