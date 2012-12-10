0260c26496ccc0d892e9906ce201fd4a1eafd5e1
Alessandro Piras <laynor@gmail.com>
2012-12-10 10:52:50 +0100

    Added job C module

--- modules/sm-module-C-job.el
+++ modules/sm-module-C-job.el
@@ -1,14 +1,14 @@
 ;;;; Module C
-(sm-module C
+(sm-module C-job
            :unmanaged-p nil
            :require-packages '(ToolBOS))
 
-(sm-module-pre (C)
+(sm-module-pre (C-job)
   ;; TODO insert your pre-package-initialization code here
   )
 
-(sm-module-post (C)
+(sm-module-post (C-job)
   ;; TODO insert your post-package-initialization code here
   )
 
-(sm-provide :module C)
+(sm-provide :module C-job)
