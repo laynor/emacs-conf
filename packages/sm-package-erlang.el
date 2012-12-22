cbce6d2d03874099fac78b4a94eac97d26520d5c
Alessandro Piras <laynor@gmail.com>
2012-12-22 01:03:29 +0100

    (packages:erlang) Do not insert a newline when inserting an arrow

--- packages/sm-package-erlang.el
+++ packages/sm-package-erlang.el
@@ -3,9 +3,17 @@
             :package-manager "package"
             :unmanaged-p nil)
 
+(evil-define-key 'insert erlang-mode-map (kbd ">")
+  (lambda ()
+    (interactive)
+    (flet ((newline () nil))
+      (call-interactively 'erlang-electric-gt))))
+
+
 (add-hook 'erlang-mode-hook
           (lambda ()
             (setq inferior-erlang-machine-options '("-sname" "emacs"))))
+
 (require 'erlang-start)
 
 
