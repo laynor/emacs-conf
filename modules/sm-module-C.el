2ab4536e2d1fbdc8891a7c645b9f0e8d3e23723d
Alessandro Piras <laynor@gmail.com>
2012-12-27 08:09:26 +0100

    Added C-eldoc, improved C module

--- modules/sm-module-C.el
+++ modules/sm-module-C.el
@@ -1,14 +1,26 @@
 ;;;; Module C
 (sm-module C
            :unmanaged-p nil
-           :require-packages '(auto-complete-clang))
+           :require-packages '(auto-complete-clang c-eldoc))
 
 (sm-module-pre (C)
-  ;; TODO insert your pre-package-initialization code here
   )
 
 (sm-module-post (C)
-  ;; TODO insert your post-package-initialization code here
+  (defvar c-get-standard-include-dirs-command
+    "echo | cpp -x c++ -Wp,-v 2>&1 | grep '^.*include' | grep -v '^\\(ignoring\\|#include\\)' | sed 's/^ //g'"
+    "Command used to retrieve the standard C/C++ include directories.")
+
+  (defun c-get-standard-include-dirs ()
+    "Retrieves the standard C/C++ include directories"
+    (mapcar (lambda (include-path)
+              (concat "-I" include-path))
+            (split-string (shell-command-to-string c-get-standard-include-dirs-command) "\n" t)))
+
+  (setq ac-clang-flags (c-get-standard-include-dirs))
+
+  (setq c-eldoc-includes (concat c-eldoc-includes " "
+                                 (mapconcat 'identity (c-get-standard-include-dirs) " ")))
   )
 
 (sm-provide :module C)
