0c269329dd283d77a85383268281f5da19176376
Alessandro Piras <laynor@gmail.com>
2012-12-27 20:00:03 +0100

    Move jedi command customization to home-linux profile

--- profiles/home-linux.el
+++ profiles/home-linux.el
@@ -13,7 +13,7 @@
 ;;; After loading modules
 (sm-profile-post (home-linux)
   (custom-set-variables
-   '(jedi:server-command (quote ("python2" "/home/ale/.emacs.d/elpa/jedi-20121212.2016/jediepcserver.py")))
+   `(jedi:server-command ("python2" ,(concat jedi:source-dir "jediepcserver.py")))
    '(python-shell-interpreter "python2"))
 
   (load "custom.el"))
