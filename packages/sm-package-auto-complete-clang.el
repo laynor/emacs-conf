99dd0eeb118ffdf79b3141de76dd414e40b03a88
Alessandro Piras <laynor@gmail.com>
2012-12-27 11:35:17 +0100

    Remove docs in auto-complete-clang menus.

--- packages/sm-package-auto-complete-clang.el
+++ packages/sm-package-auto-complete-clang.el
@@ -7,7 +7,7 @@
 
 ;; Do not insert function signature in the buffer
 (setq ac-source-clang
-      (cons '(action . nil) ac-source-clang))
+      (append '((document . nil) (action . nil)) ac-source-clang))
 
 (defadvice ac-clang-candidate (after ac-clang-candidate-show-signature-in-summary activate)
   (setq ad-return-value
