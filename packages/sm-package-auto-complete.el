a842b12fa3147aed8c8326f20c5265292643abbb
Alessandro Piras <laynor@gmail.com>
2012-12-27 19:59:33 +0100

    Auto-complete package cleanup

--- packages/sm-package-auto-complete.el
+++ packages/sm-package-auto-complete.el
@@ -4,21 +4,12 @@
             :unmanaged-p nil)
 
 (require 'auto-complete-config)
-;;(add-to-list 'ac-dictionary-directories (concat myoptdir "AC/ac-dict"))
 
-
-;; (ac-set-trigger-key "TAB")
-;; (define-key ac-mode-map  [(control tab)] 'auto-complete)
-;; (define-key ac-mode-map  [(control tab)] 'auto-complete)
 (defun my-ac-config ()
   (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
-  ;; (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
-  ;; (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
-  ;; (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
-  ;; (add-hook 'css-mode-hook 'ac-css-mode-setup)
   (add-hook 'auto-complete-mode-hook 'ac-common-setup)
   (global-auto-complete-mode t))
-;; ac-source-gtags
+
 (my-ac-config)
 
 (sm-provide :package auto-complete)
