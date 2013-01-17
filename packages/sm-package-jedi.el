f077589e966e5ec42ba69ce4c28671bd9774a9db
Alessandro Piras <laynor@gmail.com>
2013-01-17 19:45:22 +0100

    Updated jedi configuration

--- packages/sm-package-jedi.el
+++ packages/sm-package-jedi.el
@@ -3,12 +3,13 @@
             :package-manager "package"
             :unmanaged-p nil)
 
+(require 'jedi)
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
 ;; (setq jedi:tooltip-method '(pos-tip))
+(setq jedi:server-command (list "python2" jedi:server-script))
 
 (autoload 'jedi:setup "jedi" nil t)
-
 (defadvice jedi:ac-direct-matches (after jedi:remove-summary activate)
   (setq ad-return-value
         (mapcar (lambda (item)
