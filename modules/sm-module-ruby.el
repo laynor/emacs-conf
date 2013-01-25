3eda684560f1ec4968f7047f596f40d6b0605634
Alessandro Piras <laynor@gmail.com>
2013-01-25 23:31:05 +0100

    update ruby

--- modules/sm-module-ruby.el
+++ modules/sm-module-ruby.el
@@ -1,17 +1,25 @@
 ;;;; Module ruby
 (sm-module ruby
            :unmanaged-p nil
-           :require-packages '(yasnippet rsense enotify enotify-espectator))
+           :require-packages '(yasnippet rsense enotify enotify-espectator hideshow ale-fixme))
 
 (sm-module-pre (ruby)
   ;; TODO insert your pre-package-initialization code here
   )
 
 (sm-module-post (ruby)
+  ;; Folding with hideshow
+  (add-to-list 'hs-special-modes-alist
+               '(ruby-mode
+                 "\\(class\\|def\\|do\\|{\\)" "\\(end\\|end\\|}\\)" "#"
+                 (lambda (arg) (ruby-end-of-block)) nil))
+  ;; Autocomplete with rsense
   (add-hook 'ruby-mode-hook 'add-rsense-ac-sources)
   (add-hook 'ruby-mode-hook 'add-yasnippet-ac-sources)
   (add-hook 'ruby-mode-hook 'yas-minor-mode-on)
   (add-hook 'ruby-mode-hook 'yas-reload-all)
+  ;; todo/fixme comments
+  (add-hook 'ruby-mode-hook '(lambda () (fic-ext-mode 1)))
   )
 
 (sm-provide :module ruby)
---
+++ packages/sm-package-highlight-indentation.el
@@ -0,0 +1,8 @@
+;;;; Package highlight-indentation
+(sm-package highlight-indentation
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package highlight-indentation)
