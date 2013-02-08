62994d20f0d7aaea2b3292a8d63db8edca26cb14
Alessandro Piras <laynor@gmail.com>
2013-02-08 10:03:28 +0100

    Add package 'package' to module 'base'.

--- modules/sm-module-base.el
+++ modules/sm-module-base.el
@@ -9,7 +9,7 @@
                                "pp-c-l" "erc" "dired" "browse-kill-ring"
                                "popup-git" "whitespace" "gist"
                                "markdown-mode" "markdown-mode+"
-                               "auto-complete")
+                               "auto-complete" "package")
            ;; set this to t if you want to manage this module yourself
            ;; instead of using the builtin package loading infrastructure
            :unmanaged-p nil)
