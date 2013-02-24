c75132298ef17e5fa32a3d0d19cf4c716075a518
Alessandro Piras <laynor@gmail.com>
2013-02-25 00:36:25 +0100

    Added popwin package

---
+++ packages/sm-package-popwin.el
@@ -0,0 +1,8 @@
+;;;; Package popwin
+(sm-package popwin
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package popwin)
