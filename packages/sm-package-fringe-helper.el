b0ce91c588df9f49cef43b6d982dbab2164043ed
Alessandro Piras <laynor@gmail.com>
2013-02-25 11:48:21 +0100

    added git gutter packages

---
+++ packages/sm-package-fringe-helper.el
@@ -0,0 +1,8 @@
+;;;; Package fringe-helper
+(sm-package fringe-helper
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package fringe-helper)
---
+++ packages/sm-package-git-gutter-fringe.el
@@ -0,0 +1,8 @@
+;;;; Package git-gutter-fringe
+(sm-package git-gutter-fringe
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package git-gutter-fringe)
