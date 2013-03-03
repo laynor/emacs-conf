437bd61ac9ed3f5d55836facdffc164b41b4abe4
Alessandro Piras <laynor@gmail.com>
2013-03-03 22:17:33 +0100

    add package surround

---
+++ packages/sm-package-surround.el
@@ -0,0 +1,8 @@
+;;;; Package surround
+(sm-package surround
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package surround)
