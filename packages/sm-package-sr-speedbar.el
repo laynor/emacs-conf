324e2aa31195324b3e5e941f8602591c7fd87e61
Alessandro Piras <laynor@gmail.com>
2013-03-06 19:43:17 +0100

    added package sr-speedbar

---
+++ packages/sm-package-sr-speedbar.el
@@ -0,0 +1,8 @@
+;;;; Package sr-speedbar
+(sm-package sr-speedbar
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package sr-speedbar)
