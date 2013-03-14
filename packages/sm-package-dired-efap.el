f479db387c041993470085156fbf8f3783daaf1f
Alessandro Piras <laynor@gmail.com>
2013-03-14 19:39:47 +0100

    added efap and single dired addons

---
+++ packages/sm-package-dired-efap.el
@@ -0,0 +1,8 @@
+;;;; Package dired-efap
+(sm-package dired-efap
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package dired-efap)
---
+++ packages/sm-package-dired-single.el
@@ -0,0 +1,8 @@
+;;;; Package dired-single
+(sm-package dired-single
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package dired-single)
