2489c4c2536141a8bb1280be5241526b73cc67fa
Alessandro Piras <laynor@gmail.com>
2013-03-14 19:38:02 +0100

    updated projectile (clean commands)

--- packages/sm-package-projectile.el
+++ packages/sm-package-projectile.el
@@ -5,4 +5,26 @@
 
 (projectile-global-mode t)
 
+
+(defvar projectile-project-cleaning-commands
+  '(("./rebar clean" .
+     (lambda (dir)
+       (file-exists-p (expand-file-name "rebar" dir))))
+    ("rebar clean" .
+     (lambda (dir)
+       (and (executable-find "rebar")
+            (file-exists-p (expand-file-name "rebar.config" dir)))))
+    ("make clean" .
+     (lambda (dir)
+       (file-exists-p (expand-file-name "Makefile" dir))))
+    )
+  "A list of pairs of commands and prerequisite lambdas to perform project cleaning.")
+
+(defun projectile-compile-project (&optional arg)
+  "Run project compilation command."
+  (interactive "p")
+  (case arg
+    (4 (projectile-run-project-command projectile-project-cleaning-commands))
+    (otherwise (projectile-run-project-command projectile-project-compilation-commands))))
+
 (sm-provide :package projectile)
