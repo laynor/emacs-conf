50b043395cf7b91dfc3b6101a4c01bf24f3b3f21
Alessandro Piras <laynor@gmail.com>
2013-03-21 09:03:25 +0100

    Add emacs-eclim package.

---
+++ packages/sm-package-emacs-eclim.el
@@ -0,0 +1,8 @@
+;;;; Package emacs-eclim
+(sm-package emacs-eclim
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package emacs-eclim)
