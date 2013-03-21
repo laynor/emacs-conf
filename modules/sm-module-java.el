d56f92e7ec726255d8aa6167249571f54f938e48
Alessandro Piras <laynor@gmail.com>
2013-03-21 18:43:18 +0100

    added java module, smart-forward package

---
+++ modules/sm-module-java.el
@@ -0,0 +1,14 @@
+;;;; Module java
+(sm-module java
+           :unmanaged-p nil
+           :require-packages '(emacs-eclim))
+
+(sm-module-pre (java)
+  ;; TODO insert your pre-package-initialization code here
+  )
+
+(sm-module-post (java)
+  ;; TODO insert your post-package-initialization code here
+  )
+
+(sm-provide :module java)
---
+++ packages/sm-package-smart-forward.el
@@ -0,0 +1,8 @@
+;;;; Package smart-forward
+(sm-package smart-forward
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package smart-forward)
