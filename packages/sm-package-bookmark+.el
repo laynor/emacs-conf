662ece7132900013b8e69eb0bbbc0002cf7b425c
Alessandro Piras <laynor@gmail.com>
2013-03-22 19:52:53 +0100

    Add bookmark+ package

---
+++ packages/sm-package-bookmark+.el
@@ -0,0 +1,8 @@
+;;;; Package bookmark+
+(sm-package bookmark+
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package bookmark+)
