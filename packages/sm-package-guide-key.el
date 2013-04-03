f85e6996f3afb1ac674ee32b4f241cd2489a29d3
Alessandro Piras <laynor@gmail.com>
2013-04-03 11:45:32 +0200

    guide-key -> recursive-key-seq.

--- packages/sm-package-guide-key.el
+++ packages/sm-package-guide-key.el
@@ -5,6 +5,7 @@
 
 (require 'guide-key)
 (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-c p" "C-x 8"))
+(setq guide-key/recursive-key-sequence-flag t)
 (guide-key-mode 1)  ; Enable guide-key-mode
 
 (sm-provide :package guide-key)
