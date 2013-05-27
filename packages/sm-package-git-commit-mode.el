55a19dbfd8c38ba35282b734ecf80d4182c85c1c
Alessandro Piras <laynor@gmail.com>
2013-05-27 13:26:51 +0200

    Hacked git-commit-mode to make the maximum summary length customizable.

--- packages/sm-package-git-commit-mode.el
+++ packages/sm-package-git-commit-mode.el
@@ -3,6 +3,29 @@
             :package-manager "package"
             :unmanaged-p nil)
 
-;;; TODO insert your package initialization code here
+(require 'git-commit-mode)
+
+(makunbound 'git-commit-summary-regexp)
+
+(defcustom git-commit-summary-maxlen 50
+  "Summary length threshold for git summary"
+  :group 'git-commit
+  :type 'integer)
+
+(defvar git-commit-summary-regexp
+   (format "\\(?:^\\(.\\{,%d\\}\\)\\(.*?\\)$\\)"
+           git-commit-summary-maxlen)
+   "Regexp to match the summary line.
+
+Do not use this expression directly, instead call
+`git-commit-find-summary-regexp' to create a regular expression
+to match the summary line.")
+
+(defun git-commit-summary-maxlen-update ()
+  (setq git-commit-summary-regexp
+        (format "\\(?:^\\(.\\{,%d\\}\\)\\(.*?\\)$\\)"
+                git-commit-summary-maxlen)))
+
+(add-hook 'magit-log-edit-mode-hook #'git-commit-summary-maxlen-update)
 
 (sm-provide :package git-commit-mode)
