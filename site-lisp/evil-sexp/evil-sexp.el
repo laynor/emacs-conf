c7edf393fb4afa88cb24aca3bfbdc487897cd062
Alessandro Piras <laynor@gmail.com>
2013-06-09 00:26:25 +0200

    Switch to paredit.

--- site-lisp/evil-sexp/evil-sexp.el
+++ site-lisp/evil-sexp/evil-sexp.el
@@ -1,53 +1,36 @@
-;; (defun evil-sexp-enter ()
-;;   (interactive)
-;;   (cond ((and (= (char-after (point)) ?\()
-;;               (= (char-after (1+ (point))) ?\n))
-;;          (forward-char 2))
-;;         ((= (char-after (point)) ?\()
-;;          (forward-char))
-;;         ((= (char-after (point)) ?\))
-;;          (backward-char))
-;;         ))
-
-;; (define-key evil-motion-state-map (kbd "M-j") 'evil-sexp-enter)
-;; (define-key evil-motion-state-map (kbd "M-k") 'backward-up-list)
-;; (define-key evil-motion-state-map (kbd "M-h") '(lambda ()
-;;                                                  (interactive)
-;;                                                  (cond ((eq (char-after (point)) ?\))
-;;                                                         (forward-char) (backward-sexp))
-;;                                                        (t (backward-sexp)))))
-;; (define-key evil-motion-state-map (kbd "M-l") (lambda ()
-;;                                                 (interactive)
-;;                                                 (unless (= (char-after (point)) ?\()
-;;                                                   (forward-char))
-;;                                                 (forward-sexp)
-;;                                                 (backward-char)))
 (evil-define-motion evil-forward-sexp (count)
   :type inclusive
   (dotimes (i (or count 1))
-    (let ((syntax-after-point (char-syntax (char-after (point))))
+    (let ((lookahead-1 (char-syntax (char-after (point))))
+          (lookahead-2 (char-syntax (char-after (1+ (point)))))
           (new-point (point)))
       (condition-case nil
           (progn (save-excursion
-                   (unless (= syntax-after-point ?\()
-                     (forward-char))
-                   (forward-sexp)
-                   (backward-char)
-                   (setq new-point (point)))
+                   (message "lookahead1 = %S, lookahead-2 = %S"
+                            (string lookahead-1) (string lookahead-2))
+                   (cond ((or (memq lookahead-2 '(?\ ?>))
+                              (member lookahead-1 '(?\ ?>)))
+                          (forward-char)
+                          (skip-syntax-forward "->")
+                          (setq new-point (point)))
+                         (t (unless (memq lookahead-1 '(?\" ?\())
+                              (forward-char))
+                            (paredit-forward)
+                            (backward-char)
+                            (setq new-point (point)))))
                  (goto-char new-point))
         (error (error "End of sexp"))))))
-
 
 (evil-define-motion evil-backward-sexp (count)
   :type inclusive
   (dotimes (i (or count 1))
-    (let ((syntax-after-point (char-syntax (char-after (point))))
+    (let ((lookahead (char-syntax (char-after (point))))
           (new-point (point)))
       (condition-case nil
           (progn (save-excursion
-                   (when (= syntax-after-point ?\))
+                   (when (memq lookahead '(?\) ?\"))
                      (forward-char))
-                   (backward-sexp)
+                   (paredit-backward)
                    (setq new-point (point)))
                  (goto-char new-point))
         (error (error "Beginning of sexp"))))))
@@ -79,17 +62,19 @@
             (t (error "Already at the deepest level"))))))
 
 ;; Does not work correctly when there are spaces after parens
+;; does not work correctly when inside a string, check paredit.
 ;; check when there are spaces before parens
 ;; When the cursor is on an open paren, go up one level on an open paren
+(require 'paredit)
 (evil-define-motion evil-exit-sexp (count)
   :type inclusive
   (dotimes (i (or count 1))
     (let (op-pos cl-pos)
       (condition-case nil
           (progn (save-excursion
-                   (backward-up-list)
+                   (paredit-backward-up)
                    (setq op-pos (point))
-                   (forward-sexp)
+                   (paredit-forward)
                    (setq cl-pos (point)))
                  (let ((lookahead (char-syntax (char-after (point)))))
                    (case lookahead
