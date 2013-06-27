282fc648cee596e3bbca2ccbe188e735b8c40cc5
Alessandro Piras <alessandro@tekno-soft.it>
2013-06-27 11:46:53 +0200

    ido-vertical-mode: Keep minibuffer at the same height whatever the candidates.

--- packages/sm-package-ido-vertical-mode.el
+++ packages/sm-package-ido-vertical-mode.el
@@ -4,5 +4,8 @@
             :unmanaged-p nil)
 
 (ido-vertical-mode t)
+(defadvice ido-completions (after smufu (name) activate)
+  (let* ((n (length (split-string ad-return-value "\n" t))))
+    (setq ad-return-value (concat ad-return-value (s-repeat (1+ (- ido-max-prospects n)) "\n")))))
 
 (sm-provide :package ido-vertical-mode)
