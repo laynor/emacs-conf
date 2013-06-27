3314a039583cd8563ce3dac1bc5c6b9ccf1046b5
Alessandro Piras <alessandro@tekno-soft.it>
2013-06-27 19:14:59 +0200

    Add outorg and outshine submodules.

---
+++ packages/sm-package-outorg.el
@@ -0,0 +1,8 @@
+;;;; Package outorg
+(sm-package outorg
+            :package-manager nil
+            :unmanaged-p t)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package outorg)
---
+++ packages/sm-package-outshine.el
@@ -0,0 +1,8 @@
+;;;; Package outshine
+(sm-package outshine
+            :package-manager nil
+            :unmanaged-p t)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package outshine)
