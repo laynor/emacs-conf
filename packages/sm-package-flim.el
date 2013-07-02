fbb04582715278ce895e2f28c2a0666f0c5134a7
Alessandro Piras <laynor@gmail.com>
2013-07-02 09:48:27 +0200

    Add flim package.

---
+++ packages/sm-package-flim.el
@@ -0,0 +1,8 @@
+;;;; Package flim
+(sm-package flim
+            :package-manager "el-get"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package flim)
