5e20a0387e2d685b049f2801c4c5b449a249316f
Alessandro Piras <laynor@gmail.com>
2013-07-18 19:27:47 +0200

    some updates.

--- packages/sm-package-flx.el
+++ packages/sm-package-flx.el
@@ -3,10 +3,4 @@
             :package-manager "package"
             :unmanaged-p nil)
 
-(require 'flx-ido)
-;;(ido-everywhere 1)
-(flx-ido-mode 1)
-;; disable ido faces to see flx highlights.
-;; (setq ido-use-faces t)
-
 (sm-provide :package flx)
