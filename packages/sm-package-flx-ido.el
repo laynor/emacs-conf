b5fc99f555b6b4450cc926ffbca82c0b71bb3164
Alessandro Piras <laynor@gmail.com>
2013-07-21 13:32:13 +0200

    Add flx-ido package.

---
+++ packages/sm-package-flx-ido.el
@@ -0,0 +1,9 @@
+;;;; Package flx-ido
+(sm-package flx-ido
+            :package-manager "package"
+            :unmanaged-p nil)
+
+(require 'flx-ido)
+(flx-ido-mode 1)
+
+(sm-provide :package flx-ido)
