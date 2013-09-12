52a32c74fb7208da1a7e01b0363070b579d40fc3
Alessandro Piras <laynor@gmail.com>
2013-09-12 15:22:35 +0200

    quickrun js

--- packages/sm-package-quickrun.el
+++ packages/sm-package-quickrun.el
@@ -3,6 +3,13 @@
             :package-manager "package"
             :unmanaged-p nil)
 
-;;; TODO insert your package initialization code here
+(quickrun-add-command "js/d8"
+                      '((:command . "d8")
+                        (:exec    . ("%c %s")))
+                      :default "js")
+
+(quickrun-set-default "js3" "js/d8")
+(quickrun-set-default "js" "js/d8")
+(quickrun-set-default "javascript" "js/d8")
 
 (sm-provide :package quickrun)
