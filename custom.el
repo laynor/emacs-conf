5599809de843fc579ffd360cc5a0c54efaddceb3
Alessandro Piras <laynor@gmail.com>
2013-09-14 17:37:27 +0200

    custom.el: random stuff.

--- custom.el
+++ custom.el
@@ -41,7 +41,7 @@
  '(evil-default-cursor (quote (t "magenta")))
  '(evil-emacs-state-modes
    (quote
-    (direx:direx-mode eassist-mode archive-mode bbdb-mode bookmark-bmenu-mode bookmark-edit-annotation-mode browse-kill-ring-mode bzr-annotate-mode calc-mode cfw:calendar-mode completion-list-mode Custom-mode debugger-mode delicious-search-mode desktop-menu-blist-mode desktop-menu-mode doc-view-mode dvc-bookmarks-mode dvc-diff-mode dvc-info-buffer-mode dvc-log-buffer-mode dvc-revlist-mode dvc-revlog-mode dvc-status-mode dvc-tips-mode ediff-mode efs-mode Electric-buffer-menu-mode emms-browser-mode emms-mark-mode emms-metaplaylist-mode emms-playlist-mode ert-results-mode etags-select-mode fj-mode gc-issues-mode gdb-breakpoints-mode gdb-disassembly-mode gdb-frames-mode gdb-locals-mode gdb-memory-mode gdb-registers-mode gdb-threads-mode gist-list-mode gnus-article-mode gnus-browse-mode gnus-group-mode gnus-server-mode gnus-summary-mode google-maps-static-mode ibuffer-mode jde-javadoc-checker-report-mode magit-commit-mode magit-diff-mode magit-key-mode magit-log-mode magit-mode magit-reflog-mode magit-show-branches-mode magit-stash-mode magit-status-mode magit-wazzup-mode magit-key-mode mh-folder-mode monky-mode notmuch-hello-mode notmuch-search-mode notmuch-show-mode occur-mode org-agenda-mode package-menu-mode proced-mode rcirc-mode rebase-mode recentf-dialog-mode reftex-select-bib-mode reftex-toc-mode sldb-mode slime-inspector-mode slime-thread-control-mode slime-xref-mode sr-buttons-mode sr-mode sr-tree-mode sr-virtual-mode tar-mode tetris-mode tla-annotate-mode tla-archive-list-mode tla-bconfig-mode tla-bookmarks-mode tla-branch-list-mode tla-browse-mode tla-category-list-mode tla-changelog-mode tla-follow-symlinks-mode tla-inventory-file-mode tla-inventory-mode tla-lint-mode tla-logs-mode tla-revision-list-mode tla-revlog-mode tla-tree-lint-mode tla-version-list-mode twittering-mode urlview-mode vc-annotate-mode vc-dir-mode vc-git-log-view-mode vc-svn-log-view-mode vm-mode vm-summary-mode w3m-mode wab-compilation-mode xgit-annotate-mode xgit-changelog-mode xgit-diff-mode xgit-revlog-mode xhg-annotate-mode xhg-log-mode xhg-mode xhg-mq-mode xhg-mq-sub-mode xhg-status-extra-mode eclim-project-mode eclim-problems-mode wl-summary-mode wl-folder-mode)))
+    (direx:direx-mode eassist-mode archive-mode bbdb-mode bookmark-bmenu-mode bookmark-edit-annotation-mode browse-kill-ring-mode bzr-annotate-mode calc-mode cfw:calendar-mode completion-list-mode Custom-mode debugger-mode delicious-search-mode desktop-menu-blist-mode desktop-menu-mode doc-view-mode dvc-bookmarks-mode dvc-diff-mode dvc-info-buffer-mode dvc-log-buffer-mode dvc-revlist-mode dvc-revlog-mode dvc-status-mode dvc-tips-mode ediff-mode efs-mode Electric-buffer-menu-mode emms-browser-mode emms-mark-mode emms-metaplaylist-mode emms-playlist-mode ert-results-mode etags-select-mode fj-mode gc-issues-mode gdb-breakpoints-mode gdb-disassembly-mode gdb-frames-mode gdb-locals-mode gdb-memory-mode gdb-registers-mode gdb-threads-mode gist-list-mode gnus-article-mode gnus-browse-mode gnus-group-mode gnus-server-mode gnus-summary-mode google-maps-static-mode ibuffer-mode jde-javadoc-checker-report-mode magit-commit-mode magit-diff-mode magit-key-mode magit-log-mode magit-mode magit-reflog-mode magit-show-branches-mode magit-stash-mode magit-status-mode magit-wazzup-mode magit-key-mode mh-folder-mode monky-mode notmuch-hello-mode notmuch-search-mode notmuch-show-mode occur-mode org-agenda-mode package-menu-mode proced-mode rcirc-mode rebase-mode recentf-dialog-mode reftex-select-bib-mode reftex-toc-mode sldb-mode slime-inspector-mode slime-thread-control-mode slime-xref-mode sr-buttons-mode sr-mode sr-tree-mode sr-virtual-mode tar-mode tetris-mode tla-annotate-mode tla-archive-list-mode tla-bconfig-mode tla-bookmarks-mode tla-branch-list-mode tla-browse-mode tla-category-list-mode tla-changelog-mode tla-follow-symlinks-mode tla-inventory-file-mode tla-inventory-mode tla-lint-mode tla-logs-mode tla-revision-list-mode tla-revlog-mode tla-tree-lint-mode tla-version-list-mode twittering-mode urlview-mode vc-annotate-mode vc-dir-mode vc-git-log-view-mode vc-svn-log-view-mode vm-mode vm-summary-mode w3m-mode wab-compilation-mode xgit-annotate-mode xgit-changelog-mode xgit-diff-mode xgit-revlog-mode xhg-annotate-mode xhg-log-mode xhg-mode xhg-mq-mode xhg-mq-sub-mode xhg-status-extra-mode eclim-project-mode eclim-problems-mode wl-summary-mode wl-folder-mode quickrun/mode)))
  '(evil-motion-state-modes
    (quote
     (apropos-mode Buffer-menu-mode calendar-mode color-theme-mode command-history-mode compilation-mode dictionary-mode ert-results-mode help-mode Info-mode Man-mode speedbar-mode undo-tree-visualizer-mode view-mode woman-mode el-get-package-menu-mode)))
@@ -80,129 +80,129 @@
  '(safe-local-variable-values
    (quote
     ((eval progn
-	   (setenv "MANPATH"
-		   (mapconcat
-		    (function identity)
-		    (remove-duplicates
-		     (split-string
-		      (concat
-		       (getenv "MANPATH")
-		       ":" "/home/alessandro/local/opt/toolbos/man/")
-		      ":" t)
-		     :test
-		     (function equal))
-		    ":"))
-	   (add-project-directories "ToolBOSCore/2.0/include/" "ToolBOSCore/2.0/srcToolBOSCore/" "ToolBOSCore/2.0/srcLogCollector/" "/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0.x86_64/include/"))
+           (setenv "MANPATH"
+                   (mapconcat
+                    (function identity)
+                    (remove-duplicates
+                     (split-string
+                      (concat
+                       (getenv "MANPATH")
+                       ":" "/home/alessandro/local/opt/toolbos/man/")
+                      ":" t)
+                     :test
+                     (function equal))
+                    ":"))
+           (add-project-directories "ToolBOSCore/2.0/include/" "ToolBOSCore/2.0/srcToolBOSCore/" "ToolBOSCore/2.0/srcLogCollector/" "/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0.x86_64/include/"))
      (eval progn
-	   (setenv "MANPATH"
-		   (mapconcat
-		    (function identity)
-		    (remove-duplicates
-		     (split-string
-		      (concat
-		       (getenv "MANPATH")
-		       ":" "/home/alessandro/local/opt/toolbos/man/")
-		      ":" t)
-		     :test
-		     (function equal))
-		    ":"))
-	   (add-project-directories "ToolBOSCore/2.0/include/" "ToolBOSCore/2.0/srcToolBOSCore/" "ToolBOSCore/2.0/srcLogCollector/"))
+           (setenv "MANPATH"
+                   (mapconcat
+                    (function identity)
+                    (remove-duplicates
+                     (split-string
+                      (concat
+                       (getenv "MANPATH")
+                       ":" "/home/alessandro/local/opt/toolbos/man/")
+                      ":" t)
+                     :test
+                     (function equal))
+                    ":"))
+           (add-project-directories "ToolBOSCore/2.0/include/" "ToolBOSCore/2.0/srcToolBOSCore/" "ToolBOSCore/2.0/srcLogCollector/"))
      (eval progn
-	   (setenv "MANPATH"
-		   (mapconcat
-		    (function identity)
-		    (remove-duplicates
-		     (split-string
-		      (concat
-		       (getenv "MANPATH")
-		       ":"
-		       (dir-locals-directory)
-		       "/home/alessandro/local/opt/toolbos/man/")
-		      ":" t)
-		     :test
-		     (function equal))
-		    ":"))
-	   (add-project-directories "ToolBOSCore/2.0/include/" "ToolBOSCore/2.0/srcToolBOSCore/" "ToolBOSCore/2.0/srcLogCollector/"))
+           (setenv "MANPATH"
+                   (mapconcat
+                    (function identity)
+                    (remove-duplicates
+                     (split-string
+                      (concat
+                       (getenv "MANPATH")
+                       ":"
+                       (dir-locals-directory)
+                       "/home/alessandro/local/opt/toolbos/man/")
+                      ":" t)
+                     :test
+                     (function equal))
+                    ":"))
+           (add-project-directories "ToolBOSCore/2.0/include/" "ToolBOSCore/2.0/srcToolBOSCore/" "ToolBOSCore/2.0/srcLogCollector/"))
      (eval progn
-	   (setenv "MANPATH"
-		   (mapconcat
-		    (function identity)
-		    (remove-duplicates
-		     (split-string
-		      (concat
-		       (getenv "MANPATH")
-		       ":"
-		       (dir-locals-directory)
-		       "ToolBOSCore/2.0/doc/man/")
-		      ":" t)
-		     :test
-		     (function equal))
-		    ":"))
-	   (add-project-directories "ToolBOSCore/2.0/include/" "ToolBOSCore/2.0/srcToolBOSCore/" "ToolBOSCore/2.0/srcLogCollector/"))
+           (setenv "MANPATH"
+                   (mapconcat
+                    (function identity)
+                    (remove-duplicates
+                     (split-string
+                      (concat
+                       (getenv "MANPATH")
+                       ":"
+                       (dir-locals-directory)
+                       "ToolBOSCore/2.0/doc/man/")
+                      ":" t)
+                     :test
+                     (function equal))
+                    ":"))
+           (add-project-directories "ToolBOSCore/2.0/include/" "ToolBOSCore/2.0/srcToolBOSCore/" "ToolBOSCore/2.0/srcLogCollector/"))
      (eval progn
-	   (setenv "MANPATH"
-		   (mapconcat
-		    (function identity)
-		    (remove-duplicates
-		     (split-string
-		      (concat
-		       (getenv "MANPATH")
-		       ":"
-		       (dir-locals-directory)
-		       "ToolBOSCore/2.0/doc/man/")
-		      ":" t)
-		     :test
-		     (function equal))
-		    ":"))
-	   (setenv "BST_CMAKE_OPTIONS" "-DCMAKE_BUILD_TYPE=Debug")
-	   (add-project-directories "/home/alessandro/src/ToolBOSCore/ToolBOSCore/2.0/include/" "/home/alessandro/src/ToolBOSCore/ToolBOSCore/2.0/srcToolBOSCore/" "/home/alessandro/src/ToolBOSCore/ToolBOSCore/2.0/srcLogCollector/"))
+           (setenv "MANPATH"
+                   (mapconcat
+                    (function identity)
+                    (remove-duplicates
+                     (split-string
+                      (concat
+                       (getenv "MANPATH")
+                       ":"
+                       (dir-locals-directory)
+                       "ToolBOSCore/2.0/doc/man/")
+                      ":" t)
+                     :test
+                     (function equal))
+                    ":"))
+           (setenv "BST_CMAKE_OPTIONS" "-DCMAKE_BUILD_TYPE=Debug")
+           (add-project-directories "/home/alessandro/src/ToolBOSCore/ToolBOSCore/2.0/include/" "/home/alessandro/src/ToolBOSCore/ToolBOSCore/2.0/srcToolBOSCore/" "/home/alessandro/src/ToolBOSCore/ToolBOSCore/2.0/srcLogCollector/"))
      (c-file-style "hri")
      (eval progn
-	   (setenv "MANPATH"
-		   (mapconcat
-		    (function identity)
-		    (remove-duplicates
-		     (split-string
-		      (concat
-		       (getenv "MANPATH")
-		       ":"
-		       (dir-locals-directory)
-		       "ToolBOSCore/2.0/doc/man/")
-		      ":" t)
-		     :test
-		     (function equal))
-		    ":"))
-	   (setenv "BST_CMAKE_OPTIONS" "-DCMAKE_BUILD_TYPE=Debug")
-	   (add-project-directories "ToolBOSCore/2.0/include/" "ToolBOSCore/2.0/srcToolBOSCore/" "ToolBOSCore/2.0/srcLogCollector/"))
+           (setenv "MANPATH"
+                   (mapconcat
+                    (function identity)
+                    (remove-duplicates
+                     (split-string
+                      (concat
+                       (getenv "MANPATH")
+                       ":"
+                       (dir-locals-directory)
+                       "ToolBOSCore/2.0/doc/man/")
+                      ":" t)
+                     :test
+                     (function equal))
+                    ":"))
+           (setenv "BST_CMAKE_OPTIONS" "-DCMAKE_BUILD_TYPE=Debug")
+           (add-project-directories "ToolBOSCore/2.0/include/" "ToolBOSCore/2.0/srcToolBOSCore/" "ToolBOSCore/2.0/srcLogCollector/"))
      (eval add-project-directories "ToolBOSCore/2.0/include/" "ToolBOSCore/2.0/srcToolBOSCore/" "ToolBOSCore/2.0/srcLogCollector/")
      (eval progn
-	   (add-project-directories
-	    (toolbos-includes)
-	    "1.0/src/")
-	   (c-set-style "hri"))
+           (add-project-directories
+            (toolbos-includes)
+            "1.0/src/")
+           (c-set-style "hri"))
      (eval add-project-directories
-	   (toolbos-includes)
-	   "1.0/src/")
+           (toolbos-includes)
+           "1.0/src/")
      (eval setq python-shell-virtualenv-path
-	   (file-truename "~/.virtualenvs/pyls"))
+           (file-truename "~/.virtualenvs/pyls"))
      (eval setq python-shell-virtualenv-path "/Users/alessandro/.virtualenvs/pyls")
      (python-shell-virtualenv-path expand-filename "~/.virtualenvs/pyls")
      (eval add-project-directories "/include/" "/third_party/dice-pjsip/pjmedia/include" "/third_party/dice-pjsip/pjlib/include" "/third_party/dice-pjsip/pjlib-util/include" "/third_party/dice-pjsip/pjsip/include" "/third_party/dice-pjsip/pjnath/include")
      (eval progn
-	   (message "stercoraro")
-	   (add-to-list
-	    (quote ac-clang-cflags)
-	    "-I/home/ale/src/C/prova"))
+           (message "stercoraro")
+           (add-to-list
+            (quote ac-clang-cflags)
+            "-I/home/ale/src/C/prova"))
      (eval add-project-directories "include/" "third_party/dice-pjsip/pjmedia/include" "/third_party/dice-pjsip/pjlib/include" "/third_party/dice-pjsip/pjlib-util/include" "/third_party/dice-pjsip/pjsip/include" "/third_party/dice-pjsip/pjnath/include")
      (eval add-project-directories "include/")
      (eval progn
-	   (setq my-include-directories
-		 (list
-		  (concat
-		   (file-name-directory
-		    (file-truename load-file-name))
-		   "include/")))
-	   (add-my-include-directories))
+           (setq my-include-directories
+                 (list
+                  (concat
+                   (file-name-directory
+                    (file-truename load-file-name))
+                   "include/")))
+           (add-my-include-directories))
      (c-auto-newline)
      (c-file-style . k&r))))
  '(scroll-bar-mode nil)
@@ -235,11 +235,11 @@
  '(diff-removed ((t (:inherit diff-changed :background "firebrick4"))))
  '(ediff-odd-diff-C ((t (:background "Dim Grey" :foreground "White"))) t)
  '(eldoc-highlight-function-argument ((t (:inherit bold :box nil :underline "deeppink"))))
- '(factor-font-lock-comment ((t (:inherit font-lock-comment-face))) t)
- '(factor-font-lock-parsing-word ((t (:inherit font-lock-keyword-face))) t)
- '(factor-font-lock-stack-effect ((t (:foreground "green"))) t)
- '(factor-font-lock-string ((t (:inherit font-lock-string-face))) t)
- '(factor-font-lock-word ((t (:inherit font-lock-function-name-face))) t)
+ '(factor-font-lock-comment ((t (:inherit font-lock-comment-face))))
+ '(factor-font-lock-parsing-word ((t (:inherit font-lock-keyword-face))))
+ '(factor-font-lock-stack-effect ((t (:foreground "green"))))
+ '(factor-font-lock-string ((t (:inherit font-lock-string-face))))
+ '(factor-font-lock-word ((t (:inherit font-lock-function-name-face))))
  '(flx-highlight-face ((t (:inherit font-lock-keyword-face :underline t :weight bold))))
  '(flycheck-error ((t (:inherit error :underline t))))
  '(font-lock-comment-face ((t (:foreground "MediumPurple3" :slant italic))))
