6aa200ce347626dd8c14188fd579ff28a726d6d9
Alessandro Piras <laynor@gmail.com>
2013-09-21 05:49:46 +0200

    [list-packages-ext] some fixes.

--- packages/sm-package-list-packages-ext.el
+++ packages/sm-package-list-packages-ext.el
@@ -3,7 +3,7 @@
             :package-manager nil
             :unmanaged-p t)
 
-(load "~/.emacs.d/site-lisp/list-packages-ext.el")
+(load "~/.emacs.d/site-lisp/list-packages-ext.elc")
 (add-hook 'package-menu-mode-hook '(lambda () (list-packages-ext-mode 1)))
 
 (sm-provide :package list-packages-ext)
