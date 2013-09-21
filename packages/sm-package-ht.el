d737e8803c5d9218508c5345f82656daacff7023
Alessandro Piras <laynor@gmail.com>
2013-09-21 05:49:16 +0200

    Add sm-package-ht

---
+++ packages/sm-package-ht.el
@@ -0,0 +1,8 @@
+;;;; Package ht
+(sm-package ht
+            :package-manager "package"
+            :unmanaged-p nil)
+
+;;; TODO insert your package initialization code here
+
+(sm-provide :package ht)
