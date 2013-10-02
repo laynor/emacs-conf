6686ad2ddda7061d05da506e3eb5007f67118695
Alessandro Piras <laynor@gmail.com>
2013-10-03 00:13:39 +0200

    list-packages-ext package.

--- packages/sm-package-list-packages-ext.el
+++ packages/sm-package-list-packages-ext.el
@@ -3,7 +3,28 @@
             :package-manager nil
             :unmanaged-p t)
 
-(load "~/.emacs.d/site-lisp/list-packages-ext.elc")
-(add-hook 'package-menu-mode-hook '(lambda () (list-packages-ext-mode 1)))
+(add-to-list 'load-path (local-repo "list-packages-ext"))
+
+(require 'list-packages-ext)
+
+(add-hook 'package-menu-mode-hook '(lambda ()
+                                    (list-packages-ext-mode 1)
+                                    (hl-line-mode 1)))
+
+;;;;  Kludges
+
+;; (eval-after-load 'smooth-scrolling
+;;   (progn
+;;     (defun disable-smooth-scroll ()
+;;       (ad-disable-advice 'next-line 'after 'smooth-scroll-up)
+;;       (ad-disable-advice 'previous-line 'after 'smooth-scroll-down)
+;;       (ad-activate 'next-line)
+;;       (ad-activate 'previous-line))
+
+
+;;     (add-hook 'list-packages-ext-mode-hook
+;;               'disable-smooth-scroll)))
+
+
 
 (sm-provide :package list-packages-ext)
