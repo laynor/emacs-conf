319b447a4a0bd10fe9027d654174e3b3a5cbf6f1
Alessandro Piras <laynor@gmail.com>
2014-10-23 20:38:00 +0200

    update home-windows profile.

--- profiles/home-windows.el
+++ profiles/home-windows.el
@@ -2,19 +2,24 @@
 
 ;;; Before loading modules
 (sm-profile-pre (home-windows)
+  (setq flymake-mode nil)
   (setq custom-file (concat user-emacs-directory "custom.el"))
+  (setenv "PATH" (concat   "c:\\Users\\ale\\AppData\\Local\\scoop\\apps\\findutils\\4.4.2\\bin;"
+                           "c:\\Users\\ale\\AppData\\Local\\scoop\\apps\\diffutils\\2.8.7\\bin;"
+                           "c:\\Users\\ale\\AppData\\Local\\scoop\\apps\\grep\\2.5.4\\bin;"
+                           (getenv "PATH")))
   )
 
 ;;; Modules to activate
-(sm-require-modules "base" "elisp")
+(sm-require-modules "base" "elisp" "c-sharp" "javascript" "web")
 
 ;;; After loading modules
 (sm-profile-post (home-windows)
-  (setq magit-git-executable "c:/cygwin/bin/git.exe")
-  (load "custom.el")
-  (add-hook 'kill-emacs-hook 'sm-recompile-all)
+  (setq browse-url-firefox-program "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe")
+  ;; (setq magit-git-executable "c:/cygwin/bin/git.exe")
+  (load custom-file)
   (custom-set-faces
-   '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "outline" :family "Consolas"))))))
+   '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "microsoft" :family "Consolas")))))
+  )
 
-
-;;;; sm-base-profile end.
+;;;; sm-base-profile end
