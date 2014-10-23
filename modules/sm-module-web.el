80fb0d358ceff50b3c20b4c9c8b00c3068c9d7c0
Alessandro Piras <laynor@gmail.com>
2014-10-23 20:36:59 +0200

    Use web-mode for html files.

--- modules/sm-module-web.el
+++ modules/sm-module-web.el
@@ -1,10 +1,11 @@
 ;;;; Module web
 (sm-module web
            :unmanaged-p nil
-           :require-packages '(nxhtml zencoding))
+           :require-packages '(web-mode zencoding))
 
 (sm-module-pre (web)
   ;; TODO insert your pre-package-initialization code here
+  (add-to-list 'auto-mode-alist (cons "\\.html?$" 'web-mode))
   )
 
 (sm-module-post (web)
